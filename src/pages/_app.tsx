69f6c699d1ae91c8f71ed7f3a4f1a866558e97c1
Renato Lomba <rntlomba@gmail.com>
2022-08-17 22:57:22 -0300

    Base layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,36 @@
 import type { AppProps } from 'next/app'
 
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        background: 'gray.900',
+        color: 'gray.100',
+        lineHeight: 'tall',
+        fontSize: '2xl',
+
+        '@media screen and (max-width: 480px)': {
+          fontSize: 'lg',
+        },
+      },
+
+      '::selection': {
+        color: 'white',
+        background: 'purple.500',
+        '-webkit-text-stroke': '0',
+      },
+    },
+  },
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,83 @@
 import type { NextPage } from 'next'
 
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+} from '@chakra-ui/react'
+
 const Home: NextPage = () => {
-  return <h1>Hello world</h1>
+  return (
+    <Flex
+      alignItems="center"
+      maxWidth="780px"
+      w="100%"
+      mx="auto"
+      h="100vh"
+      px="4"
+    >
+      <Box
+        as="main"
+        w="100%"
+        bgColor="gray.700"
+        p={{ base: '5', md: '8' }}
+        borderRadius="md"
+      >
+        <Flex as="header" mx="auto" width="fit-content">
+          <Heading
+            size={{ base: 'lg', sm: 'xl', md: '2xl' }}
+            color="yellow.500"
+          >
+            Py
+          </Heading>
+          <Heading size={{ base: 'lg', sm: 'xl', md: '2xl' }} color="black">
+            Next
+          </Heading>
+          <Heading size={{ base: 'lg', sm: 'xl', md: '2xl' }} color="red.500">
+            Tube
+          </Heading>
+        </Flex>
+
+        <Flex
+          as="form"
+          mt="10"
+          flexDir="column"
+          alignItems="flex-start"
+          gap="8"
+        >
+          <FormControl isInvalid>
+            <FormLabel>Url do vídeo: </FormLabel>
+
+            <Input
+              _placeholder={{
+                color: 'gray.400',
+              }}
+              variant="flushed"
+              focusBorderColor="purple.500"
+              placeholder="https://youtube.com/watch?v="
+            />
+
+            <FormErrorMessage fontSize="lg" fontWeight="semibold">
+              teste
+            </FormErrorMessage>
+          </FormControl>
+
+          <Button
+            w={{ base: '100%', sm: 'fit-content' }}
+            type="submit"
+            colorScheme="purple"
+          >
+            Download
+          </Button>
+        </Flex>
+      </Box>
+    </Flex>
+  )
 }
 
 export default Home
